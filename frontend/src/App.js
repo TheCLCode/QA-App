3bddbf86a589f349b55f556dbcce439fcd1812d8
Christian Leroy Castillo <castich@oocl.com>
2018-11-26 16:34:31 +0800

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import NavBar from './NavBar/NavBar';
+import Questions from './Questions/Questions';
+import Question from './Question/Question';
+import {Route} from 'react-router-dom';
+
+class App extends Component {
+  render() {
+    return (<div>
+      <NavBar/>
+      <Route exact path='/' component={Questions}/>
+      <Route exact path='/question/:questionId' component={Question}/>
+    </div>
+    );
+  }
+}
+
+export default App;
