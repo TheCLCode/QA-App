086a055ee818cc48ec67c5bba418d8e640d69ad9
Christian Leroy Castillo <castich@oocl.com>
2018-11-29 16:51:43 +0800

    Add more codes based on tutorial

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,6 +4,8 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const jwt = require('express-jwt');
+const jwksRsa = require('jwks-rsa');
 
 // define the Express app
 const app = express();
@@ -54,6 +56,21 @@ app.get('/:id', (req, res) => {
   if (question.length === 0) return res.status(404).send();
   res.send(question[0]);
 });
+
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://theclcode.auth0.com/.well-known/jwks.json`
+  }),
+
+  // Validate the audience and the issuer.
+  audience: 'OYUdtZYPSdUH4I67QEvKC4CPWo5HiqVg',
+  issuer: `https://theclcode.auth0.com/`,
+  algorithms: ['RS256']
+});
+
 
 // insert a new question
 app.post('/', (req, res) => {
