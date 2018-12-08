0374cdb69326ae9ae4d00992c4eab5381ecfb776
Christian <castich@oocl.com>
2018-12-08 16:23:59 +0800

    fix

--- src/config/env.js
+++ src/config/env.js
@@ -1,4 +1,11 @@
-var URL = "https://theclcode-qandapp.herokuapp.com";
+const hostname = window && window.location && window.location.hostname;
+var URL = "";
+if(hostname==="herokuapp.com"){
+	URL = "http://theclcode-qandapp.herokuapp.com";
+} else{
+	URL = process.env.REACT_APP_BACKEND_HOST || "http://localhost:8081";
+}
+console.log("URL");
 export const config = {
 	URL
 };
